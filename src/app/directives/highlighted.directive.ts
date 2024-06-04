0d15b431d8fc46fcb645aa78c1777f3ac92a66c0
Manoel Lima <manoelsslima@yahoo.com.br>
2024-06-04 01:31:19 -0400

    Angular @HostBinding in Detail - DOM Properties vs Attributes.

--- src/app/directives/highlighted.directive.ts
+++ src/app/directives/highlighted.directive.ts
@@ -1,4 +1,4 @@
-import { Directive } from '@angular/core';
+import { Directive, HostBinding, Input } from '@angular/core';
 
 @Directive({
   selector: '[highlighted]',
@@ -6,8 +6,26 @@ import { Directive } from '@angular/core';
 })
 export class HighlightedDirective {
 
+  @Input('highlighted')
+  isHightlighted = false;
+
   constructor() {
     console.log('Directive created...');
+  }
+
+  /**
+   * 
+   */
+  // @HostBinding('className') // à qual propriedade do DOM queremos atribuir esse valor
+  //   get cssClasses() { // nome que escolhemos
+  //   return "highlighted"; // css que queremos aplicar ao elemento host dessa diretiva
+  // }
+  /**
+   * Vincula o host a um novo método
+   */
+  @HostBinding('class.highlighted') // à qual propriedade do DOM queremos atribuir esse valor
+  get cssClasses() { // nome que escolhemos
+    return this.isHightlighted; // css que queremos aplicar ao elemento host dessa diretiva
   }
 
 }
