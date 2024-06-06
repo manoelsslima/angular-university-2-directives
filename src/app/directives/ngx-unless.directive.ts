9cf7d6a491c84c2b5ff400cb212a816e0441793a
Manoel Lima <manoelsslima@yahoo.com.br>
2024-06-06 01:34:14 -0400

    Angular Strucutral Directives - Step-by-Step Implementation

--- src/app/directives/ngx-unless.directive.ts
+++ src/app/directives/ngx-unless.directive.ts
@@ -1,4 +1,4 @@
-import { Directive } from '@angular/core';
+import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
 
 @Directive({
   selector: '[ngxUnless]',
@@ -6,6 +6,25 @@ import { Directive } from '@angular/core';
 })
 export class NgxUnlessDirective {
 
-  constructor() { }
+  visible = false; // criada para evitar remover o viewContainer
+
+  constructor(private templateRef: TemplateRef<any>,
+    private viewContainer: ViewContainerRef
+  ) { }
+
+  /**
+   * Esse método vai criar um novo template no lugar da tag <ng-template />
+   */
+  @Input()
+  set ngxUnless(condition: boolean) {
+    if (!condition && !this.visible) {
+      // instanciando o template
+      this.viewContainer.createEmbeddedView(this.templateRef);
+      this.visible = true;
+    } else if (condition && this.visible) {
+      this.viewContainer.clear();
+      this.visible = false;
+    }
+  }
 
 }
